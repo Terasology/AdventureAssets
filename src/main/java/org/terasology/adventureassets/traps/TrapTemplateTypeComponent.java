b9ee1868954687591e6aa4e7fca595c7b687ddad
Nihal Singh <nihal.111@gmail.com>
2017-06-08 23:02:06 +0530

    Created the trapPlaceholder

---
+++ src/main/java/org/terasology/adventureassets/traps/TrapTemplateTypeComponent.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.adventureassets.traps;
+
+import org.terasology.entitySystem.Component;
+import org.terasology.logic.common.DisplayNameComponent;
+import org.terasology.structureTemplates.components.ScheduleStructurePlacementComponent;
+import org.terasology.structureTemplates.components.StructureTemplateComponent;
+
+/**
+ * Only used as marker component to allow the search for prefabs that describe trap template types.
+ *
+ * Prefabs with the {@link TrapTemplateTypeComponent} should also have a {@link DisplayNameComponent} so
+ * that they can be listed with proper name and description in editor UIs.
+ */
+public class TrapTemplateTypeComponent implements Component {
+}
--- src/main/java/org/terasology/adventureassets/traps/swingingblade/SpawnSwingingBladeServerSystem.java
+++ src/main/java/org/terasology/adventureassets/traps/swingingblade/SpawnSwingingBladeServerSystem.java
@@ -15,19 +15,33 @@
  */
 package org.terasology.adventureassets.traps.swingingblade;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.adventureassets.traps.RequestTrapPlaceholderPrefabSelection;
+import org.terasology.adventureassets.traps.TrapPlaceholderComponent;
 import org.terasology.assets.management.AssetManager;
 import org.terasology.entitySystem.entity.EntityBuilder;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.EventPriority;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.CharacterComponent;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.math.geom.Quat4f;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.In;
+import org.terasology.structureTemplates.components.AddItemsToChestComponent;
+import org.terasology.structureTemplates.components.ScheduleStructurePlacementComponent;
+import org.terasology.structureTemplates.events.BuildStructureTemplateEntityEvent;
+import org.terasology.structureTemplates.events.SpawnTemplateEvent;
 import org.terasology.structureTemplates.events.StructureBlocksSpawnedEvent;
+import org.terasology.structureTemplates.internal.components.StructurePlaceholderComponent;
+import org.terasology.structureTemplates.internal.events.BuildStructureTemplateStringEvent;
+import org.terasology.structureTemplates.internal.events.RequestStructurePlaceholderPrefabSelection;
+import org.terasology.structureTemplates.util.transform.BlockRegionTransform;
 import org.terasology.world.WorldProvider;
 
 
@@ -44,12 +58,29 @@ public class SpawnSwingingBladeServerSystem extends BaseComponentSystem {
     @In
     private WorldProvider worldProvider;
 
+    private static final Logger logger = LoggerFactory.getLogger(SpawnSwingingBladeServerSystem.class);
+
     @ReceiveEvent
     public void onSpawnStructureWithSwingingBlade(StructureBlocksSpawnedEvent event, EntityRef entity,
                                                   SpawnSwingingBladeComponent component) {
+        spawnSwingingBlades(event.getTransformation(), component);
+    }
+
+    @ReceiveEvent
+    public void onTemplateSpawned(SpawnTemplateEvent event, EntityRef entity, SpawnSwingingBladeComponent component) {
+        spawnSwingingBlades(event.getTransformation(), component);
+    }
+
+    @ReceiveEvent
+    public void onBuildTemplateStringWithBlockRegions(BuildStructureTemplateStringEvent event, EntityRef template,
+                                                      SpawnSwingingBladeComponent component) {
+        logger.info("works");
+    }
+
+    private void spawnSwingingBlades(BlockRegionTransform transformation, SpawnSwingingBladeComponent component) {
         for (SpawnSwingingBladeComponent.SwingingBlade swingingBlade : component.bladeList) {
-            Vector3i position = event.getTransformation().transformVector3i(swingingBlade.position);
-            Quat4f rotation = event.getTransformation().transformRotation(swingingBlade.rotation);
+            Vector3i position = transformation.transformVector3i(swingingBlade.position);
+            Quat4f rotation = transformation.transformRotation(swingingBlade.rotation);
 
             EntityBuilder entityBuilder = entityManager.newBuilder(swingingBlade.prefab);
             LocationComponent locationComponent = entityBuilder.getComponent(LocationComponent.class);
@@ -62,6 +93,24 @@ public class SpawnSwingingBladeServerSystem extends BaseComponentSystem {
 
             entityBuilder.build();
         }
+    }
 
+    @ReceiveEvent
+    public void onBuildTemplateWithScheduledStructurePlacment(BuildStructureTemplateEntityEvent event, EntityRef entity) {
+    }
+
+    @ReceiveEvent
+    public void onRequestTrapPlaceholderPrefabSelection(RequestTrapPlaceholderPrefabSelection event, EntityRef characterEntity,
+                                                             CharacterComponent characterComponent) {
+        EntityRef interactionTarget = characterComponent.authorizedInteractionTarget;
+        TrapPlaceholderComponent trapPlaceholderComponent = interactionTarget.getComponent(TrapPlaceholderComponent.class);
+        if (trapPlaceholderComponent == null) {
+            logger.error("Ignored RequestTrapPlaceholderPrefabSelection event since there was no interaction with a trap placeholder");
+            return;
+        }
+
+        trapPlaceholderComponent.selectedPrefab = event.getPrefab();
+        interactionTarget.saveComponent(trapPlaceholderComponent);
+        logger.info("updated trap placeholder to: " + event.getPrefab().getName());
     }
 }
